fa23a6ee24c16d752b79d9b0d8c1f7703d3fee79
Oscar Bergström <oscar.bergstrom96@gmail.com>
2024-06-03 10:42:05 +0200

    first commit

---
+++ CharacterStructure/DOD.js
@@ -0,0 +1,10 @@
+var characterStructure = {
+        "Styrka": 0,
+        "Smidighet": 0,
+        "Tålighet": 0,
+        "Intelligens": 0,
+        "Psyke": 0,
+        "Vilja": 0,
+        "Karisma": 0,
+
+};
